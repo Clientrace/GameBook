eb26d14a47641bca22ae0f1392b93a8f8077c3ca
pkimclarence@yahoo.com.ph <pkimclarence@yahoo.com.ph>
2016-09-21 22:40:06 +0800

    create accounts table and users_info table

---
+++ database/migrations/2016_09_21_142944_create_users_info_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersInfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_info', function (Blueprint $table) {
+            $table->integer('user_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->integer('experience');
+            $table->date('birthdate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_info');
+    }
+}
